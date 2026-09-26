2b2a65e4961f4ab6da65caa6bc99a0d7adba0218
Myo Khant Kyaw <myokhantkyaw.developer@gmail.com>
2026-09-26 16:48:56 +0630

    product

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -10,16 +10,12 @@ import {
   deleteProduct,
 } from "../controllers/product.controller";
 
-import {
-  authenticate,
-  authorize,
-} from "../middleware/auth";
 
 const router = Router();
 
 /*
 |--------------------------------------------------------------------------
-| CREATE
+| CREATE PRODUCT
 |--------------------------------------------------------------------------
 | POST /api/products
 |--------------------------------------------------------------------------
@@ -27,14 +23,13 @@ const router = Router();
 
 router.post(
   "/",
-  authenticate,
-  authorize("OWNER", "ADMIN", "MANAGER"),
+ 
   createProduct
 );
 
 /*
 |--------------------------------------------------------------------------
-| GET ALL
+| GET ALL PRODUCTS
 |--------------------------------------------------------------------------
 | GET /api/products
 | GET /api/products?keyword=milk
@@ -43,13 +38,13 @@ router.post(
 
 router.get(
   "/",
-  authenticate,
+ 
   getProducts
 );
 
 /*
 |--------------------------------------------------------------------------
-| GET ONE
+| GET PRODUCT BY ID
 |--------------------------------------------------------------------------
 | GET /api/products/:id
 |--------------------------------------------------------------------------
@@ -57,13 +52,13 @@ router.get(
 
 router.get(
   "/:id",
-  authenticate,
+  
   getProductById
 );
 
 /*
 |--------------------------------------------------------------------------
-| UPDATE
+| UPDATE PRODUCT
 |--------------------------------------------------------------------------
 | PUT /api/products/:id
 |--------------------------------------------------------------------------
@@ -71,8 +66,7 @@ router.get(
 
 router.put(
   "/:id",
-  authenticate,
-  authorize("OWNER", "ADMIN", "MANAGER"),
+ 
   updateProduct
 );
 
@@ -87,7 +81,6 @@ router.put(
 router.patch(
   "/:id",
   authenticate,
-  authorize("OWNER", "ADMIN", "MANAGER"),
   updateProductStock
 );
 
@@ -101,14 +94,13 @@ router.patch(
 
 router.patch(
   "/:id/status",
-  authenticate,
-  authorize("OWNER", "ADMIN", "MANAGER"),
+ 
   updateProductStatus
 );
 
 /*
 |--------------------------------------------------------------------------
-| DELETE
+| DELETE PRODUCT
 |--------------------------------------------------------------------------
 | DELETE /api/products/:id
 |--------------------------------------------------------------------------
@@ -116,8 +108,7 @@ router.patch(
 
 router.delete(
   "/:id",
-  authenticate,
-  authorize("OWNER", "ADMIN"),
+  
   deleteProduct
 );
 
